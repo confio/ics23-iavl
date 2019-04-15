2e20913fcc455507744454a2353458daa53c1fbd
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-04-15 23:38:32 +0200

    Decypher amino encoding, got simplest test case passing

--- convert.go
+++ convert.go
@@ -18,6 +18,9 @@ func ConvertExistenceProof(p *iavl.RangeProof, key, value []byte) (*proofs.Exist
 	}
 
 	leaf := convertLeafOp(p.Leaves[0].Version)
+	leafHash := p.Leaves[0].Hash()
+	fmt.Printf("leaf hash: %X\n", leafHash)
+
 	inner := convertInnerOps(p.LeftPath)
 	// prepend leaf
 	steps := append([]*proofs.ProofOp{leaf}, inner...)
@@ -32,9 +35,9 @@ func ConvertExistenceProof(p *iavl.RangeProof, key, value []byte) (*proofs.Exist
 
 func convertLeafOp(version int64) *proofs.ProofOp {
 	// this is adapted from iavl/proof.go:proofLeafNode.Hash()
-	prefix := encodeVarInt(0)
-	prefix = append(prefix, encodeVarInt(1)...)
-	prefix = append(prefix, encodeVarInt(version)...)
+	prefix := aminoVarInt(0)
+	prefix = append(prefix, aminoVarInt(1)...)
+	prefix = append(prefix, aminoVarInt(version)...)
 
 	leaf := &proofs.LeafOp{
 		Hash:         proofs.HashOp_SHA256,
@@ -42,6 +45,7 @@ func convertLeafOp(version int64) *proofs.ProofOp {
 		Length:       proofs.LengthOp_VAR_PROTO,
 		Prefix:       prefix,
 	}
+	fmt.Printf("Leaf: %#v\n", leaf)
 	return proofs.WrapLeaf(leaf)
 }
 
@@ -53,9 +57,9 @@ func convertInnerOps(path iavl.PathToLeaf) []*proofs.ProofOp {
 	// we want to go up from the leaf to the root
 	for i := len(path) - 1; i >= 0; i-- {
 		// this is adapted from iavl/proof.go:proofInnerNode.Hash()
-		prefix := encodeVarInt(int64(path[i].Height))
-		prefix = append(prefix, encodeVarInt(path[i].Size)...)
-		prefix = append(prefix, encodeVarInt(path[i].Version)...)
+		prefix := aminoVarInt(int64(path[i].Height))
+		prefix = append(prefix, aminoVarInt(path[i].Size)...)
+		prefix = append(prefix, aminoVarInt(path[i].Version)...)
 
 		var suffix []byte
 		if len(path[i].Left) > 0 {
@@ -76,13 +80,21 @@ func convertInnerOps(path iavl.PathToLeaf) []*proofs.ProofOp {
 	return steps
 }
 
-func encodeVarInt(l int64) []byte {
+func aminoVarInt(orig int64) []byte {
+	// amino-specific byte swizzling
+	i := uint64(orig) << 1
+	if orig < 0 {
+		i = ^i
+	}
+
 	// avoid multiple allocs for normal case
 	res := make([]byte, 0, 8)
-	for l >= 1<<7 {
-		res = append(res, uint8(l&0x7f|0x80))
-		l >>= 7
+
+	// standard protobuf encoding
+	for i >= 1<<7 {
+		res = append(res, uint8(i&0x7f|0x80))
+		i >>= 7
 	}
-	res = append(res, uint8(l))
+	res = append(res, uint8(i))
 	return res
 }
--- convert_test.go
+++ convert_test.go
@@ -6,17 +6,17 @@ import (
 	"testing"
 
 	"github.com/tendermint/iavl"
-	cmn "github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/db"
 )
 
 func TestConvertProof(t *testing.T) {
-	proof, err := generateRangeProof(200)
+	proof, err := generateRangeProof(1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	fmt.Println(proof.proof.String())
+	fmt.Printf("Root: %X\n", proof.rootHash)
 
 	converted, err := ConvertExistenceProof(proof.proof, proof.key, proof.value)
 	if err != nil {
@@ -48,21 +48,30 @@ func generateRangeProof(size int) (*provenValue, error) {
 	tree := iavl.NewMutableTree(db.NewMemDB(), 0)
 
 	// insert lots of info and store the bytes
-	allkeys := make([][]byte, size)
-	for i := 0; i < size; i++ {
-		key := cmn.RandStr(20)
-		value := "value_for_" + key
-		tree.Set([]byte(key), []byte(value))
-		allkeys[i] = []byte(key)
-	}
+	/*
+		allkeys := make([][]byte, size)
+		for i := 0; i < size; i++ {
+			key := cmn.RandStr(20)
+			value := "value_for_" + key
+			tree.Set([]byte(key), []byte(value))
+			allkeys[i] = []byte(key)
+		}
 
-	key := allkeys[0]
+		key := allkeys[0]
+	*/
+	key := []byte{0xca, 0xfe}
+	val := []byte{0xde, 0xad, 0xbe, 0xef}
+	tree.Set(key, val)
+
 	value, proof, err := tree.GetWithProof(key)
 	if err != nil {
 		return nil, err
 	}
 	if value == nil {
 		return nil, fmt.Errorf("GetWithProof returned nil value")
+	}
+	if !bytes.Equal(value, val) {
+		return nil, fmt.Errorf("Wrote %X got value %X", val, value)
 	}
 	if len(proof.Leaves) != 1 {
 		return nil, fmt.Errorf("GetWithProof returned %d leaves", len(proof.Leaves))
